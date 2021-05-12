6f5ee3f14c2b6be63d5543a2ab04113afe40ad61
vishalicious213 <vishalicious213@gmail.com>
2021-05-12 16:48:09 -0700

    added element selectors in script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+// 1. select all elements that we'll work with
+const form = document.querySelector('#new-item-form')
+const list = document.querySelector('#list')
+const input = document.querySelector('#item-input')
+
+// 2. Add a new element when submit is clicked
