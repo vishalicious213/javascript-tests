1e615276c4f088e2aa5c4f77f7180af9da2d74df
vishalicious213 <vishalicious213@gmail.com>
2021-05-12 16:52:38 -0700

    script.js: added event listener and set up steps for functionality

--- script.js
+++ script.js
@@ -4,3 +4,13 @@ const list = document.querySelector('#list')
 const input = document.querySelector('#item-input')
 
 // 2. Add a new element when submit is clicked
+form.addEventListener('submit', e => {
+    e.preventDefault()
+
+    // console.log(input.value)
+
+    // 1. create a new item
+    // 2. add item to the list
+    // 3. clear input to prep for new item
+    // 4. setup event listener to delete item when clicked
+})
