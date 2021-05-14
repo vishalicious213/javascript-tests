b4c4f3d1f5006ab16f822884387313636777d2de
vishalicious213 <vishalicious213@gmail.com>
2021-05-13 17:47:22 -0700

    modal: improved event listeners & extracted closeModal code

--- modal/script.js
+++ modal/script.js
@@ -21,14 +21,28 @@ openBtn.addEventListener('click', () => {
 
 // TODO: 4. Create a click event listener for the close-modal-btn that removes the class "open" from the modal
 // BONUS: Also remove the class "open" from the overlay
-closeBtn.addEventListener('click', () => {
-  modal.classList.remove('open')
-  overlay.classList.remove('open')
-  // console.log('close')
-})
+// closeBtn.addEventListener('click', () => {
+//   modal.classList.remove('open')
+//   overlay.classList.remove('open')
+//   // console.log('close')
+// })
 
 // BONUS: Add a click event listener to the overlay that removes the class "open" from the modal and the overlay
-overlay.addEventListener('click', () => {
+// overlay.addEventListener('click', () => {
+//   modal.classList.remove('open')
+//   overlay.classList.remove('open')
+// })
+
+
+// BETTER WAY THAN TODO 4 AND BONUS, ABOVE:
+// both the close button and the overlay listen for a click
+// when either are clicked, they remove the 'open' class from both
+// closeModal is extracted and can be updated and affect both elements
+// the event listeners call closeModal on click, its the 2nd parameter
+closeBtn.addEventListener('click', closeModal)
+overlay.addEventListener('click', closeModal)
+
+function closeModal() {
   modal.classList.remove('open')
   overlay.classList.remove('open')
-})
+}
