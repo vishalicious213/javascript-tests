70addd2980ab753944b8749a75fbee52e3409e45
vishalicious213 <vishalicious213@gmail.com>
2021-05-13 17:25:38 -0700

    modal: added click eventlistneer to close button & overlay

--- modal/script.js
+++ modal/script.js
@@ -14,12 +14,17 @@ const overlay = document.querySelector('#overlay')
 // TODO: 3. Create a click event listener for the open-modal-btn that adds the class "open" to the modal
 // BONUS: Also add the class "open" to the overlay
 openBtn.addEventListener('click', () => {
-  openBtn.classList.add('open')
+  modal.classList.add('open')
   overlay.classList.add('open')
-  console.log('open')
+  // console.log('open')
 })
 
 // TODO: 4. Create a click event listener for the close-modal-btn that removes the class "open" from the modal
 // BONUS: Also remove the class "open" from the overlay
+closeBtn.addEventListener('click', () => {
+  modal.classList.remove('open')
+  overlay.classList.remove('open')
+  // console.log('close')
+})
 
 // BONUS: Add a click event listener to the overlay that removes the class "open" from the modal and the overlay
